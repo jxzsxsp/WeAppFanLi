fdd77dac14a3470e163ee877a20ee74c544e7159
sean.xiao <sean.xiao@lianshang.com>
2017-02-08 15:58:32 +0800

    修改图标布局

--- pages/home/home.js
+++ pages/home/home.js
@@ -41,105 +41,105 @@ Page({
         duration: 1000,
         hotIconList: [
             {
-                img: "/images/icon/shop/v5_8_584118f0b4c7f.png",
+                img: "/images/icon/shop/taobao.png",
                 title: "淘宝"
             },
             {
-                img: "/images/icon/shop/v5_8_5841191194575.png",
+                img: "/images/icon/shop/jd.png",
                 title: "京东"
             },
             {
-                img: "/images/icon/shop/v5_8_584119c0a5404.png",
+                img: "/images/icon/shop/vip1.png",
                 title: "品牌特卖"
             },
             {
-                img: "/images/icon/shop/v5_8_584119d59dafa.png",
+                img: "/images/icon/shop/1haodian1.png",
                 title: "网上超市"
             },
             {
-                img: "/images/icon/shop/v5_8_585c7f85257c5.png",
+                img: "/images/icon/shop/eleme.png",
                 title: "外卖"
             },
             {
-                img: "/images/icon/shop/v5_8_58411a20a7bda.png",
+                img: "/images/icon/shop/ka.png",
                 title: "信用卡"
             },
             {
-                img: "/images/icon/shop/v5_8_5872ec044dcaf.png",
+                img: "/images/icon/shop/xiecheng.png",
                 title: "机/酒/门票"
             },
             {
-                img: "/images/icon/shop/v5_8_58411afacb3b8.png",
+                img: "/images/icon/shop/meituan.png",
                 title: "吃喝玩乐"
             },
             {
-                img: "/images/icon/shop/v5_8_58411a8625b6d.png",
+                img: "/images/icon/shop/mobile.png",
                 title: "充值中心"
             },
             {
-                img: "/images/icon/shop/v5_8_58411a565d147.png",
+                img: "/images/icon/shop/all.png",
                 title: "全网商城"
             }
         ],
         iconList: [
             {
-                img: "/images/icon/shop/v5_8_5847a4636f178.png",
+                img: "/images/icon/shop/haitao.png",
                 title: "海淘尖货"
             },
             {
-                img: "/images/icon/shop/v5_8_4c9481e4c8073e85.png",
+                img: "/images/icon/shop/suning.png",
                 title: "苏宁"
             },
             {
-                img: "/images/icon/shop/v5_8_58411e4794d30.png",
+                img: "/images/icon/shop/1haodian.png",
                 title: "1号店"
             },
             {
-                img: "/images/icon/shop/v5_8_9feff5ba48402883.png",
+                img: "/images/icon/shop/kaola.png",
                 title: "网易考拉"
             },
             {
-                img: "/images/icon/shop/v5_8_4a818184ebb1cc0d.png",
+                img: "/images/icon/shop/gome.png",
                 title: "国美"
             },
             {
-                img: "/images/icon/shop/v5_8_d58a54e94377e6ae.png",
+                img: "/images/icon/shop/dangdang.png",
                 title: "当当网"
             },
             {
-                img: "/images/icon/shop/v5_8_50ba3f7cba1e6c53.png",
+                img: "/images/icon/shop/amazon.png",
                 title: "亚马逊"
             },
             {
-                img: "/images/icon/shop/v5_8_6baeb75112f09fa1.png",
+                img: "/images/icon/shop/yanxuan.png",
                 title: "网易严选"
             },
             {
-                img: "/images/icon/shop/v5_8_efc3b337800875b5.png",
+                img: "/images/icon/shop/tongcheng.png",
                 title: "同程旅游"
             },
             {
-                img: "/images/icon/shop/v5_8_584123c525450.png",
+                img: "/images/icon/shop/meituan1.png",
                 title: "蘑菇街"
             },
             {
-                img: "/images/icon/shop/v5_8_584118f0b4c7f.png",
+                img: "/images/icon/shop/letv.png",
                 title: "1药网"
             },
             {
-                img: "/images/icon/shop/v5_8_584118f0b4c7f.png",
+                img: "/images/icon/shop/feiniu.png",
                 title: "艺龙旅行"
             },
             {
-                img: "/images/icon/shop/v5_8_81a21445772454cb.png",
+                img: "/images/icon/shop/huawei.png",
                 title: "华为商城"
             },
             {
-                img: "/images/icon/shop/v5_8_585c80a5ec38c.png",
+                img: "/images/icon/shop/tmall.png",
                 title: "酒仙网"
             },
             {
-                img: "/images/icon/shop/v5_8_58412ef63d2a8.png",
+                img: "/images/icon/shop/more.png",
                 title: "更多"
             }
         ]
