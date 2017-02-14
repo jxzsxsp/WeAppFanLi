bf7901c8f6d09513ccf49c86e35c4f1ea7e212de
sean.xiao <sean.xiao@lianshang.com>
2017-02-14 12:37:22 +0800

    完善首页，添加充值中心子页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -42,123 +42,179 @@ Page({
         hotIconList: [
             {
                 img: "/images/icon/shop/taobao.png",
-                title: "淘宝"
+                title: "淘宝",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/jd.png",
-                title: "京东"
+                title: "京东",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/vip1.png",
-                title: "品牌特卖"
+                title: "品牌特卖",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/1haodian1.png",
-                title: "网上超市"
+                title: "网上超市",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/eleme.png",
-                title: "外卖"
+                title: "外卖",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/ka.png",
-                title: "信用卡"
+                title: "信用卡",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/xiecheng.png",
-                title: "机/酒/门票"
+                title: "机/酒/门票",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/meituan.png",
-                title: "吃喝玩乐"
+                title: "吃喝玩乐",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/mobile.png",
-                title: "充值中心"
+                title: "充值中心",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/all.png",
-                title: "全网商城"
+                title: "全网商城",
+                tap: "gotoAllShop"
             }
         ],
         iconList: [
             {
                 img: "/images/icon/shop/haitao.png",
-                title: "海淘尖货"
+                title: "海淘尖货",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/suning.png",
-                title: "苏宁"
+                title: "苏宁",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/1haodian.png",
-                title: "1号店"
+                title: "1号店",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/kaola.png",
-                title: "网易考拉"
+                title: "网易考拉",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/gome.png",
-                title: "国美"
+                title: "国美",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/dangdang.png",
-                title: "当当网"
+                title: "当当网",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/amazon.png",
-                title: "亚马逊"
+                title: "亚马逊",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/yanxuan.png",
-                title: "网易严选"
+                title: "网易严选",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/tongcheng.png",
-                title: "同程旅游"
+                title: "同程旅游",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/meituan1.png",
-                title: "蘑菇街"
+                title: "蘑菇街",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/letv.png",
-                title: "1药网"
+                title: "1药网",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/feiniu.png",
-                title: "艺龙旅行"
+                title: "艺龙旅行",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/huawei.png",
-                title: "华为商城"
+                title: "华为商城",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/tmall.png",
-                title: "酒仙网"
+                title: "酒仙网",
+                tap: "gotoMobilePay"
             },
             {
                 img: "/images/icon/shop/more.png",
-                title: "更多"
+                title: "更多",
+                tap: "gotoAllShop"
             }
         ]
     },
     gotoBanner1: function () {
         wx.navigateTo({
-          url: 'banner/banner1/banner1',
-          success: function(res){
-            // success
-            console.log("success")
-          },
-          fail: function() {
-            // fail
-            console.log("fail")
-          },
-          complete: function() {
-            // complete
-            console.log("complete")
-          }
+            url: 'banner/banner1/banner1',
+            success: function (res) {
+                // success
+                console.log("success")
+            },
+            fail: function () {
+                // fail
+                console.log("fail")
+            },
+            complete: function () {
+                // complete
+                console.log("complete")
+            }
+        })
+    },
+    gotoMobilePay: function () {
+        wx.navigateTo({
+            url: 'mobile/pay',
+            success: function (res) {
+                // success
+                console.log("success")
+            },
+            fail: function () {
+                // fail
+                console.log("fail")
+            },
+            complete: function () {
+                // complete
+                console.log("complete")
+            }
+        })
+    },
+    gotoAllShop: function () {
+        wx.switchTab({
+            url: '../shop/shop',
+            success: function (res) {
+                // success
+            },
+            fail: function () {
+                // fail
+            },
+            complete: function () {
+                // complete
+            }
         })
     }
 })
